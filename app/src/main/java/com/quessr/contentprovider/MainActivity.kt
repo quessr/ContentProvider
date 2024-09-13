2e2f4a56e5a6685491db28db50a3100844fbc36d
quessr <heosera76@gmail.com>
2024-09-13 10:33:13 +0900

    연락처 및 캘린더 권한 요청 및 데이터 조회 기능 추가

--- app/src/main/java/com/quessr/contentprovider/MainActivity.kt
+++ app/src/main/java/com/quessr/contentprovider/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.quessr.contentprovider
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.provider.ContactsContract
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
@@ -14,26 +18,55 @@ import com.quessr.contentprovider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var requestLauncher: ActivityResultLauncher<Intent>
+    private lateinit var contactRequestLauncher: ActivityResultLauncher<Intent>
+    private lateinit var calendarRequestLauncher: ActivityResultLauncher<Intent>
 
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
-        if (status == PackageManager.PERMISSION_GRANTED) {
-            Log.d("test", "permission granted")
-        } else {
-            ActivityCompat.requestPermissions(
+//        val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
+//        if (status == PackageManager.PERMISSION_GRANTED) {
+//            Log.d("test", "contact permission // granted")
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                this,
+//                arrayOf<String>("android.permission.READ_CONTACTS"),
+//                100
+//            )
+//            Log.d("test", "contact permission // denied")
+//        }
+//
+//        val calStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR)
+//        if (calStatus == PackageManager.PERMISSION_GRANTED) {
+//            Log.d("test", "calendar permission / granted")
+//        } else {
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CALENDAR), 200)
+//            Log.d("test", "calendar permission / denied")
+//        }
+
+        // 연락처 권한 요청
+        if (ContextCompat.checkSelfPermission(
                 this,
-                arrayOf<String>("android.permission.READ_CONTACTS"),
-                100
-            )
-            Log.d("test", "permission denied")
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 100)
         }
 
-        requestLauncher =
+        // 캘린더 권한 요청
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CALENDAR
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CALENDAR), 200)
+        }
+
+        // 연락처
+        contactRequestLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == RESULT_OK) {
                     val cursor = contentResolver.query(
@@ -56,11 +89,66 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+        calendarRequestLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val eventUri = it.data?.data
+                    if (eventUri != null) {
+                        val cursor = contentResolver.query(
+                            eventUri,
+                            arrayOf(
+                                CalendarContract.Events._ID,
+                                CalendarContract.Events.TITLE,
+                                CalendarContract.Events.DTSTART,
+                                CalendarContract.Events.DTEND
+                            ), null, null, null
+                        )
+
+                        cursor?.use {
+                            while (it.moveToNext()) {
+                                val id = it.getLong(it.getColumnIndex(CalendarContract.Events._ID))
+                                val title =
+                                    it.getString(it.getColumnIndex(CalendarContract.Events.TITLE))
+                                val startTime =
+                                    it.getLong(it.getColumnIndex(CalendarContract.Events.DTSTART))
+                                val endTime =
+                                    it.getLong(it.getColumnIndex(CalendarContract.Events.DTEND))
+
+                                Log.d(
+                                    "CalendarEvent",
+                                    "ID: $id, Title: $title, Start: $startTime, End: $endTime"
+                                )
+                            }
+                        }
+
+                    }
+                }
+            }
+
         binding.btnOpenContract.setOnClickListener {
             val intent =
-//                Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
-                Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Contactables.CONTENT_URI)
-            requestLauncher.launch(intent)
+                Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            contactRequestLauncher.launch(intent)
+        }
+
+        binding.btnOpenCalendar.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_CALENDAR
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.d("btnOpenCalender", "btnOpenCalender")
+                CalendarManager.getEvents(this) { events ->
+                    binding.tvCalendarInfo.text = events
+                    Log.d("btnOpenCalender@@@", "events $events")
+                }
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_CALENDAR),
+                    200
+                )
+            }
         }
     }
 
@@ -70,10 +158,46 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d("test", "permission granted")
-        } else {
-            Log.d("test", "permission denied")
+        when (requestCode) {
+            100 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("test", "Contact permission granted")
+                } else {
+                    Log.d("test", "Contact permission denied")
+                }
+            }
+
+            200 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("test", "Calendar permission granted")
+                } else {
+                    Log.d("test", "Calendar permission denied")
+                }
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun queryCalendarEvents() {
+        val cursor = contentResolver.query(
+            CalendarContract.Events.CONTENT_URI,
+            arrayOf(
+                CalendarContract.Events._ID,
+                CalendarContract.Events.TITLE,
+                CalendarContract.Events.DTSTART,
+                CalendarContract.Events.DTEND
+            ), null, null, null
+        )
+
+        cursor?.use {
+            while (it.moveToNext()) {
+                val id = it.getLong(it.getColumnIndex(CalendarContract.Events._ID))
+                val title = it.getString(it.getColumnIndex(CalendarContract.Events.TITLE))
+                val startTime = it.getLong(it.getColumnIndex(CalendarContract.Events.DTSTART))
+                val endTime = it.getLong(it.getColumnIndex(CalendarContract.Events.DTEND))
+
+                Log.d("CalendarEvent", "ID: $id, Title: $title, Start: $startTime, End: $endTime")
+            }
         }
     }
 }
